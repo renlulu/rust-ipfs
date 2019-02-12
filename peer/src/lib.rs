056235be34814e26279a6d0204cb153243562729
xiaohuo <xiaohuo200@gmail.com>
2019-02-12 15:12:03 +0800

    added peer eoth multihash multiaddres

--- peer/src/lib.rs
+++ peer/src/lib.rs
@@ -1,4 +1,18 @@
-pub struct Peer {}
+extern crate multiaddr;
+
+use multiaddr::Multiaddr;
+use multihash::Hash;
+
+pub struct Peer {
+    pub id: Hash,
+    pub addressed: Vec<Multiaddr>,
+}
+
+impl Peer {
+    fn add_address(&mut self, address: Multiaddr) {
+        self.addressed.push(address)
+    }
+}
 
 pub struct PeerBook {}
 
